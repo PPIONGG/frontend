64a96b814b4443685df51cc9995eb2ef7ee9d729
Ping Sorn <>
2023-05-28 09:15:09 +0700

    complete frontend

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../model/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  url: string = 'http://localhost:3000/post'
+  constructor(private http: HttpClient) { }
+
+  //get all post data
+  getAllPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.url)
+  }
+  //get post data by id
+  getPostById(id: string): Observable<Post> {
+    return this.http.get<Post>(this.url + '/' + id)
+  }
+  //Delete data by data
+  deletePost(id: string): Observable<Post> {
+    return this.http.delete<Post>(this.url + '/' + id)
+  }
+  //update post data
+  updatePost(post: Post): Observable<Post> {
+    return this.http.put<Post>(this.url + '/' + post._id, post)
+  }
+  //Post data
+  CreatePost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.url, post)
+  }
+}
